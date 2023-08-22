26acf08062e1fc361b3e2ec2f4b1097df808e971
IvanMCarval <ivanmcarval@hotmail.com>
2023-08-22 11:09:59 -0300

    Criação do projeto

---
+++ src/main/java/com/funtec/MeusContatosAPI/MeusContatosApiApplication.java
@@ -0,0 +1,13 @@
+package com.funtec.MeusContatosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusContatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusContatosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funtec/MeusContatosAPI/MeusContatosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funtec.MeusContatosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusContatosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
