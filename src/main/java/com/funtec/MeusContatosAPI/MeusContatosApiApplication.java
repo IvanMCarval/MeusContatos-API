6656d8347f2509c34ebce870d4aab017238cc2d9
IvanMCarval <ivanmcarval@hotmail.com>
2023-09-13 17:32:30 -0300

    Removido configuração do cors

--- src/main/java/com/funtec/MeusContatosAPI/MeusContatosApiApplication.java
+++ src/main/java/com/funtec/MeusContatosAPI/MeusContatosApiApplication.java
@@ -7,13 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class MeusContatosApiApplication implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*");
-    }
+public class MeusContatosApiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeusContatosApiApplication.class, args);
